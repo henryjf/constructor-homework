a4d62d5ae2abc69b276ca11e45fba16275323cfc
JEFF HENRY <henry_jf@hotmail.com>
2016-05-03 13:35:20 -0400

    added traits to dogs and humans

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,2 +1,29 @@
-let Dog = function () {}
+let Dog = function (traitsPassed) {
+
+  let traits = traitsPassed || {};
+
+  this.name = traits.name;
+  this.status = traits.happy;
+  this.color = traits.black;
+}
+
+
+// function testRunner (sadie, moonshine, atticus, mason, julia) {
+//
+//   it("should make Sadie happy when Mason pets her", function(){
+//     expect(sadie.status).toBe('normal');
+//     mason.pet(sadie);
+//     expect(sadie.status).toBe('happy');
+//   });
+
+
+
+
+
+
+
+
+
+
+
 export default Dog;
--- src/js/constructors/human.js
+++ src/js/constructors/human.js
@@ -1,2 +1,18 @@
-let Human = function () {}
+let Human = function (passedTraits) {
+  let traits = passedTraits || {};;
+
+  this.name = traits.name;
+  this.owner= traits.owner;
+  
+
+}
+
+
+// function testRunner (sadie, moonshine, atticus, mason, julia) {
+//
+//   it("should make Sadie happy when Mason pets her", function(){
+//     expect(sadie.status).toBe('normal');
+//     mason.pet(sadie);
+//     expect(sadie.status).toBe('happy');
+//   });
 export default Human;
